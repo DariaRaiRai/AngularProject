96f92e07c2d17b000b716eb8a8cf502f4b2c677f
vado <vadvoker@gmail.com>
2021-10-14 17:24:31 +0800

    add country service with temporary local storage usage

--- src/app/countries.service.ts
+++ src/app/countries.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Country } from './country';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getCountries() {
+    const savedCountries = localStorage.getItem('countries');
+    if (savedCountries) {
+      return of(JSON.parse(savedCountries));
+    } else {
+      return this.http
+        .get<Country[]>('https://corona.lmao.ninja/v2/countries')
+        .pipe(
+          map((countries: Country[]) => {
+            const countriesList = countries.map((c) => c.country);
+            localStorage.setItem('countries', JSON.stringify(countriesList));
+            return countriesList;
+          })
+        );
+    }
+  }
 }
