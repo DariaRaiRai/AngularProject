154f7d538ecdb3997d21e5d0fc9fd887906f0c7f
vado <vadvoker@gmail.com>
2021-10-14 20:27:48 +0800

    countries service ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CountriesService } from './countries.service';
-import { Country } from './country';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  countries: Observable<string[]>;
+export class AppComponent {
   title = 'my-app';
 
-  constructor(private countriesService: CountriesService) {}
-
-  ngOnInit() {
-    this.countries = this.countriesService.getCountries()
-  }
+  constructor() {}
 }
--- src/app/countries.service.ts
+++ src/app/countries.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from './country';
-import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -11,11 +10,7 @@ export class CountriesService {
   constructor(private http: HttpClient) {}
 
   getCountries() {
-    const savedCountries = localStorage.getItem('countries');
-    if (savedCountries) {
-      return of(JSON.parse(savedCountries));
-    } else {
-      return this.http
+    return this.http
         .get<Country[]>('https://corona.lmao.ninja/v2/countries')
         .pipe(
           map((countries: Country[]) => {
@@ -24,6 +19,5 @@ export class CountriesService {
             return countriesList;
           })
         );
-    }
   }
 }
